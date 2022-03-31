cef21af59b1eb0badba23b63591e246f1109d78b
greenhandzdl <greenhandzdl@gmail.com>
2022-03-31 11:17:48 +0800

    1

--- app/src/main/java/com/greenhandzdl/easytools/MainActivity.kt
+++ app/src/main/java/com/greenhandzdl/easytools/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainactivity)
+        mabutton.setOnClickListener{
+            Toast.makeText(this,"Hello World",Toast.LENGTH_SHORT).show()
+        }
     }
 }
