da3e03b3241dae55250fb2087ffd6bc9f18fade9
greenhandzdl <greenhandzdl@gmail.com>
2022-04-02 18:21:12 +0800

    Nerver Gonna Give You Up

--- app/src/main/java/com/greenhandzdl/easytools/MainActivity.kt
+++ app/src/main/java/com/greenhandzdl/easytools/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.greenhandzdl.easytools
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainactivity)
         buttonfma.setOnClickListener{
-            Toast.makeText(this,"Hello World",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"Congratulations!It's my first version." +
+                    "Nerve Gonna Give You Up",Toast.LENGTH_SHORT).show()
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.bilibili.com/video/BV1GJ411x7h7?spm_id_from=333.337.search-card.all.click")
+            startActivity(intent)
         }
     }
 }
