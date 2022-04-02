759f3562ac81b21dd8272c80be6f5a8fff12feff
greenhandzdl <greenhandzdl@gmail.com>
2022-04-02 17:05:54 +0800

    导入了一些包简化工作量

--- app/src/main/java/com/greenhandzdl/easytools/MainActivity.kt
+++ app/src/main/java/com/greenhandzdl/easytools/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.greenhandzdl.easytools
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.mainactivity.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainactivity)
-        mabutton.setOnClickListener{
+        buttonfma.setOnClickListener{
             Toast.makeText(this,"Hello World",Toast.LENGTH_SHORT).show()
         }
     }
